60c57f81cd2ab22258af818acb0491990eb0290f
harshit-malik25813 <harshitfr25@gmail.com>
2026-07-02 18:05:08 +0530

    Updated functions to match an improved style

--- functions.py
+++ functions.py
@@ -2,51 +2,48 @@ def maths():
     hw = input("What is the Maths homework?")
     final = ""
     if hw:
-        final = f"*Maths*: {hw}"
+        final = f"*Maths*: {hw}\n"
     return final
 def phy():
     hw = input("What is the Physics homework?")
     final = ""
     if hw:
-        final = f"*Physics*: {hw}"
+        final = f"*Physics*: {hw}\n"
     return final
 def chem():
     hw = input("What is the Chemistry homework?")
     final = ""
     if hw:
-        final = f"*Chemistry*: {hw}"
+        final = f"*Chemistry*: {hw}\n"
     return final
 def bio():
     hw = input("What is the bio homework?")
     final = ""
     if hw:
-        final = f"*Biology*: {hw}"
+        final = f"*Biology*: {hw}\n"
     return final
 def sst():
     hw = input("What is the SST homework?")
     final = ""
     if hw:
-        final = f"*SST*: {hw}"
+        final = f"*SST*: {hw}\n"
     return final
 def hindi():
     hw = input("What is the Hindi homework?")
     final = ""
     if hw:
-        final = f"*Hindi*: {hw}"
+        final = f"*Hindi*: {hw}\n"
     return final
 def eng():
     hw = input("What is the English homework?")
     final = ""
     if hw:
-        final = f"*English*: {hw}"
+        final = f"*English*: {hw}\n"
     return final
-def date():
-    date_string = input("Enter the date: ")
-    if date_string == "":
-        date_string = date()
-    date_string = f"*{date_string}*"
-    return date_string
 def additional():
     additional = input("Tell any additional details needed: ")
     return additional
+def text_bold(Str: str):
+    return f"*{str}*"
+
 
