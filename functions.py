6cdf674818eb70a2b7715f7c3dea8465733b4ca9
harshit-malik25813 <harshitfr25@gmail.com>
2026-07-02 18:09:39 +0530

    Refactored Whatsapp bold Asterisks

--- functions.py
+++ functions.py
@@ -2,48 +2,48 @@ def maths():
     hw = input("What is the Maths homework?")
     final = ""
     if hw:
-        final = f"*Maths*: {hw}\n"
+        final = f"{text_bold("Maths")}: {hw}\n"
     return final
 def phy():
     hw = input("What is the Physics homework?")
     final = ""
     if hw:
-        final = f"*Physics*: {hw}\n"
+        final = f"{text_bold("Physics")}: {hw}\n"
     return final
 def chem():
     hw = input("What is the Chemistry homework?")
     final = ""
     if hw:
-        final = f"*Chemistry*: {hw}\n"
+        final = f"{text_bold("Chemistry")}: {hw}\n"
     return final
 def bio():
     hw = input("What is the bio homework?")
     final = ""
     if hw:
-        final = f"*Biology*: {hw}\n"
+        final = f"{text_bold("Biology")}: {hw}\n"
     return final
 def sst():
     hw = input("What is the SST homework?")
     final = ""
     if hw:
-        final = f"*SST*: {hw}\n"
+        final = f"{text_bold("SST")}: {hw}\n"
     return final
 def hindi():
     hw = input("What is the Hindi homework?")
     final = ""
     if hw:
-        final = f"*Hindi*: {hw}\n"
+        final = f"{text_bold("Hindi")}: {hw}\n"
     return final
 def eng():
     hw = input("What is the English homework?")
     final = ""
     if hw:
-        final = f"*English*: {hw}\n"
+        final = f"{text_bold("English")}: {hw}\n"
     return final
 def additional():
     additional = input("Tell any additional details needed: ")
     return additional
-def text_bold(Str: str):
-    return f"*{str}*"
+def text_bold(Str):
+    return f"*{Str}*"
 
 
